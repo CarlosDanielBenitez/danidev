1d929afc7c8d329487bc668834704b0ab9f6d7ce
CarlosDanielBenitez <cdanibenz@gmail.com>
2023-09-12 19:06:36 -0300

    Features: added taget blanck in repositories and pages web

--- src/projects/index.jsx
+++ src/projects/index.jsx
@@ -4,53 +4,53 @@ export const Card = () => {
     return (
         <div className='layoutCard'>
             <div>
-                <a href='https://mercadosimple.netlify.app/' >Mercado - Simple</a>
-                <a href="https://github.com/CarlosDanielBenitez/mercado-simple.git">GitHub</a>
+                <a target="blanck" href='https://mercadosimple.netlify.app/' >Mercado - Simple</a>
+                <a target="blanck" href="https://github.com/CarlosDanielBenitez/mercado-simple.git">GitHub</a>
             </div>
 
             <div>
-                <a href='https://robotsman.netlify.app' >Robots - Man</a>
-                <a href="https://github.com/CarlosDanielBenitez/robots-man.git">GitHub</a>
+                <a target="blanck" href='https://robotsman.netlify.app' >Robots - Man</a>
+                <a target="blanck" href="https://github.com/CarlosDanielBenitez/robots-man.git">GitHub</a>
             </div>
 
             <div>
-                <a href='https://flocken.netlify.app' >Flocken</a>
-                <a href="https://github.com/CarlosDanielBenitez/flocken.git">GitHub</a>
+                <a target="blanck" href='https://flocken.netlify.app' >Flocken</a>
+                <a target="blanck" href="https://github.com/CarlosDanielBenitez/flocken.git">GitHub</a>
             </div>
 
             <div>
-                <a href='https://cardofcredit.netlify.app' >Card</a>
-                <a href="https://github.com/CarlosDanielBenitez/card.git">GitHub</a>
+                <a target="blanck" href='https://cardofcredit.netlify.app' >Card</a>
+                <a target="blanck" href="https://github.com/CarlosDanielBenitez/card.git">GitHub</a>
             </div>
 
             <div>
-                <a href='https://resultssummary33.netlify.app' >Summary</a>
-                <a href="https://github.com/CarlosDanielBenitez/results-summary.git">GitHub</a>
+                <a target="blanck" href='https://resultssummary33.netlify.app' >Summary</a>
+                <a target="blanck" href="https://github.com/CarlosDanielBenitez/results-summary.git">GitHub</a>
             </div>
             <div>
-                <a href='https://bellarobba.com.ar' >BellaRobba</a>
-                <a href="https://github.com/CarlosDanielBenitez/BellaRobba.git">GitHub</a>
-            </div>
-
-            <div>
-                <a href='https://calculators33.netlify.app' >Calculator</a>
-                <a href="https://github.com/CarlosDanielBenitez/mobile-calculator.git">GitHub</a>
+                <a target="blanck" href='https://bellarobba.com.ar' >BellaRobba</a>
+                <a target="blanck" href="https://github.com/CarlosDanielBenitez/BellaRobba.git">GitHub</a>
             </div>
 
             <div>
-                <a href='https://chosingadream.netlify.app' >Chosing A Dream</a>
-                <a href="https://github.com/CarlosDanielBenitez/chosing-a-dream.git">GitHub</a>
+                <a target="blanck" href='https://calculators33.netlify.app' >Calculator</a>
+                <a target="blanck" href="https://github.com/CarlosDanielBenitez/mobile-calculator.git">GitHub</a>
+            </div>
+
+            <div>
+                <a target="blanck" href='https://chosingadream.netlify.app' >Chosing A Dream</a>
+                <a target="blanck" href="https://github.com/CarlosDanielBenitez/chosing-a-dream.git">GitHub</a>
             </div>
 
 
             <div>
-                <a href='https://flying-high.netlify.app' >Flying High</a>
-                <a href="https://github.com/CarlosDanielBenitez/flying-high.git">GitHub</a>
+                <a target="blanck" href='https://flying-high.netlify.app' >Flying High</a>
+                <a target="blanck" href="https://github.com/CarlosDanielBenitez/flying-high.git">GitHub</a>
             </div>
 
             <div>
-                <a href='https://socialize33.netlify.app' >Socialize</a>
-                <a href="https://github.com/CarlosDanielBenitez/socialize.git">GitHub</a>
+                <a target="blanck" href='https://socialize33.netlify.app' >Socialize</a>
+                <a target="blanck" href="https://github.com/CarlosDanielBenitez/socialize.git">GitHub</a>
             </div>
 
 
